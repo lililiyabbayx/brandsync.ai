0d20c40b6d2c482dee87dc601655275122861129
lililiyabbayx <farhinulfatanikaa@gmail.com>
2024-09-09 20:36:55 +0600

    text2

--- app/head.tsx
+++ app/head.tsx
@@ -1,15 +1,27 @@
-import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 export default function Head() {
   return (
-    <>
-      <GoogleAnalytics measurementId="G-3PC8W1F8RH" />
+    <ClerkProvider>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3PC8W1F8RH`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3PC8W1F8RH');
+        `}
+      </Script>
       <title>Rebuilding Linear.app</title>
       <meta
         name="description"
         content="Rebuilding Linear.app's website with Next.js, Tailwind."
       />
       <meta name="robots" content="noindex" />
-    </>
+    </ClerkProvider>
   );
 }
